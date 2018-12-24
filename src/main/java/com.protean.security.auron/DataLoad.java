fc48dfd9a5ae5cb939512cdf641bf0501f42ccbf
DESKTOP-MUAA09M\JayDamon <jay.n.damon@gmail.com>
2018-12-23 19:19:20 -0500

    Added DataLoad for parent projects

---
+++ src/main/java/com.protean.security.auron/DataLoad.java
@@ -0,0 +1,24 @@
+package com.protean.security.auron;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+@Component
+public class DataLoad {
+
+    private DataSource dataSource;
+
+    public DataLoad(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public static void loadInitialStartData(DataSource dataSource) {
+        Resource resource = new ClassPathResource("data-test.sql");
+        ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator(resource);
+        databasePopulator.execute(dataSource);
+    }
+}
